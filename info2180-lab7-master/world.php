22a6bf6e4d517b5c9c252b99cb4b594db24e522f
Tiffany Garrick <tiffany.garrick@mymona.uwi.edu>
2019-11-21 04:53:41 +0000

    Final Additions

--- info2180-lab7-master/world.php
+++ info2180-lab7-master/world.php
@@ -31,3 +31,26 @@ else{
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
 </ul>
+
+<!--- Exercise 4 Not working
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Continent</th>
+    <th>Independence</th>
+    <th>Head of State</th>
+  </tr>
+  START PHP
+      $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+      $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%','%$continent%', '%$independence_year%', '%$head_of_state%'");
+      $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+          echo "<tr><td>" . $row["country"]. "</td><td>" . $row["continent"] . "</td><td>". $row["independence_year"]. "</td><td>". $row["head_of_state"]. "</td></tr>";
+        }
+        echo "</table>";
+      }
+      $conn->close();
+    END PHP
+    </table>
+--->
